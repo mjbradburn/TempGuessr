5f890cbf3df3f59ce288c5ee5f0ca168799e16da
Mark J Bradburn <bradburn@Marks-MacBook-Pro.local>
2015-06-05 01:24:20 -0400

    Initial Commit

---
+++ TempGuessrTests/TempGuessrTests.swift
@@ -0,0 +1,36 @@
+//
+//  TempGuessrTests.swift
+//  TempGuessrTests
+//
+//  Created by Mark J Bradburn on 6/5/15.
+//  Copyright (c) 2015 Mark J Bradburn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TempGuessrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
